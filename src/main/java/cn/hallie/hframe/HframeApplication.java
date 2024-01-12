8153150b511fb43a4ff36dc9dd25117dccdfb5f6
houlj12 <houlj12@lenovo.com>
2024-01-12 15:05:36 +0800

    Initial Commit

---
+++ src/main/java/cn/hallie/hframe/HframeApplication.java
@@ -0,0 +1,13 @@
+package cn.hallie.hframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HframeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HframeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/hallie/hframe/HframeApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hallie.hframe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HframeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
